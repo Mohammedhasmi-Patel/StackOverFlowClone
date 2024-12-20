110923c34f4bcbc03837039bff7adab19078bb0a
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2024-12-20 11:54:29 +0530

    complete navbar

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+  <title>Stack OverFlow</title>
+  <?php include "./client/commonFiles.php"?>
+</head>
+<body>
+    <?php include "./client/header.php"?>
+  
+</body>
+</html>
